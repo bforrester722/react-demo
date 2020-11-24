934a72f8b0347c58dcae3be81fbdebafb866bcbb
bforrester722 <b.forrester@live.com>
2020-11-24 16:45:44 -0600

    updates manifest to correct start_url

--- src/index.js
+++ src/index.js
@@ -14,25 +14,13 @@ ReactDOM.render(
 );
 
 
-// if ("serviceWorker" in navigator) {
-//   navigator.serviceWorker
-//     .register("./sw.js")
-//     .then(function(registration) {
-//       console.log("Registration successful,  is:", registration.scope);
-//     })
-//     .catch(function(err) {
-//       console.log("Service worker registration failed, error:", err);
-//     });
-// }
+
 
 // module.hot.accept();
 
 if ("serviceWorker" in navigator) {
-  console.log('asdf')
   window.addEventListener("load", () => {
     const wb = new Workbox("/sw.js");    
     wb.register();
-    console.log(wb)
   });
-
 }
